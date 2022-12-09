ee1b50623da34041d3602fd521fd3eea73008a81
Данил <iadanilko@yandex.ru>
2022-12-09 13:11:21 +0300

    12.04

--- app.py
+++ app.py
@@ -45,31 +45,36 @@ fake_database = {'users': [
         "id": 1,
         "name": "Anna",
         "nick": "Anny42",
-        "balance": 15.01
+        "balance": 15300
     },
     {
         "id": 2,
         "name": "Dima",
         "nick": "dimon2319",
-        "balance": 8.01
+        "balance": 160.23
     },
     {
         "id": 3,
         "name": "Vladimir",
         "nick": "vova777",
-        "balance": "23"
+        "balance": "25000"
     }
 ], }
 
 
 @api.get('/get_info_by_user_id/{id:int}')
 def get_info_about_user(id):
-    return fake_database['users'][id-1]
+    return fake_database['users'][id - 1]
 
 
 @api.get('/get_user_balance_by_id/{id:int}')
 def get_user_balance(id):
-    return fake_database['users'][id-1]['balance']
+    return fake_database['users'][id - 1]['balance']
 
 
-
+@api.get('/get_total_balance')
+def get_total_balance():
+    total_balance: float = 0.0
+    for user in fake_database['users']:
+        total_balance += pydantic_models.User(**user).balance
+    return total_balance
--- pydantic_models.py
+++ pydantic_models.py
@@ -1,8 +1,8 @@
 import pydantic
 
 
-# class User(pydantic.BaseModel):
-#     id: int
-#     name: str
-#     nick: str
-#     balance: float
+class User(pydantic.BaseModel):
+    id: int
+    name: str
+    nick: str
+    balance: float
