12b0ebed4ded2fa91d3857f61f8440e1587ac7ca
Данил <iadanilko@yandex.ru>
2022-12-09 12:04:25 +0300

    12.04

---
+++ app.py
@@ -0,0 +1,75 @@
+import fastapi
+import database
+import pydantic_models
+import config
+
+api = fastapi.FastAPI()
+
+response = {"Ответ": "Который возвращает сервер"}
+
+
+@api.get('/static/path')
+def hello():
+    return "hello"
+
+
+@api.get('/user/{nick}')
+def get_nick(nick):
+    return {"user": nick}
+
+
+@api.get('/userid/{id : int}')
+def get_id():
+    return {"user": "id"}
+
+
+@api.get('/user_id/{id}')
+def get_id2(id: int):
+    return {"user": id}
+
+
+@api.get('/user_id_str/{id}')
+def get_id2(id):
+    return {"user": id}
+
+
+@api.get('/test/{id:int}/{text:str}/{custom_path:path}')
+def get_test(id, text, custom_path):
+    return {"id": id,
+            "": text,
+            "custom_path": custom_path}
+
+
+fake_database = {'users': [
+    {
+        "id": 1,
+        "name": "Anna",
+        "nick": "Anny42",
+        "balance": 15.01
+    },
+    {
+        "id": 2,
+        "name": "Dima",
+        "nick": "dimon2319",
+        "balance": 8.01
+    },
+    {
+        "id": 3,
+        "name": "Vladimir",
+        "nick": "vova777",
+        "balance": "23"
+    }
+], }
+
+
+@api.get('/get_info_by_user_id/{id:int}')
+def get_info_about_user(id):
+    return fake_database['users'][id-1]
+
+
+@api.get('/get_user_balance_by_id/{id:int}')
+def get_user_balance(id):
+    return fake_database['users'][id-1]['balance']
+
+
+
